4f9b59bfb99598ec5152c3da935c771d68d0da3a
Dhimas <dhimas.adiyasa@gmail.com>
2017-08-16 17:38:38 +0700

    Sauce labs

---
+++ selenium-saucelabs/features/support/env.rb
@@ -0,0 +1,20 @@
+require 'cucumber/rails'
+require "sauce/cucumber"
+require "sauce/connect"
+
+Capybara.run_server = false
+Capybara.register_driver(:selenium){ |app| Capybara::Selenium::Driver.new(app, {:browser => :chrome })}
+#Capybara.default_driver = :selenium
+Capybara.default_driver = :sauce
+Capybara.javascript_driver = :sauce
+#Capybara.server_port = 80
+
+Sauce.config do |c|
+  c[:sauce_connect_4_executable] = "/home/dhimas/Selenium-Workspace/selenium-sauce/selenium-saucelabs/sc-4.4.8-linux/bin/sc"
+  c[:start_tunnel] = true
+  c[:skip_connection_test] = true
+#  c[:start_local_application] = false
+#  c[:application_host] = "ondemand.saucelabs.com"
+#  c[:application_port] = "80"
+  c[:browsers] = ["Windows 10", "Chrome", "60"]
+end
